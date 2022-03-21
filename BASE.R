89ebcf389c1d1095cf228a2570a733d75f3a723d
Dan Quesnelle <d.quesnelle@queensu.ca>
2022-03-21 18:07:55 -0400

    added documentation

--- BASE.R
+++ BASE.R
@@ -10,25 +10,25 @@ RandSeq = function(FileName) {
   library(stringr)
   library(littler)
   
-  File = read_file(file = paste(FileName, ".seq", sep = ""))
+  File = read_file(file = paste(FileName, ".seq", sep = "")) # Here we read the .seq files made in DNAgen.py
   
-  Seq2 = gsub(">Random Sequence ", "", File)
+  Seq2 = gsub(">Random Sequence ", "", File) # the FASTA header is removed from the file
 
-  
+  # Here we are counting the number of each base using str_count
   num_A = str_count(Seq2, "A")
   num_T = str_count(Seq2, "T")
   num_C = str_count(Seq2, "C")
   num_G = str_count(Seq2, "G")
   
   
-  counts = paste(c(num_A, num_G, num_C, num_T), collapse = ", ")
+  counts = paste(c(num_A, num_G, num_C, num_T), collapse = ", ") # the 4 counts are pasted into one object separated by ","
 
-  write_file(counts, file = paste(FileName, ".count", sep = ""))
+  write_file(counts, file = paste(FileName, ".count", sep = "")) # the counts are written into a new file called FileName.count
   
   
 }
 
 args = commandArgs(trailingOnly = TRUE)
-RandSeq(args[1])
+RandSeq(args[1]) # here we define our FileName parameter to be used in the command line
 
 
--- DNAnalyze.sh
+++ DNAnalyze.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-echo "A", "G", "C", "T" > DNAseq.csv
+echo "A", "G", "C", "T" > DNAseq.csv # here we are creating a new file called DNAseq.csv with the first line being our 4 bases
 
-for count in {1..100}
+for count in {1..100} 
 do
 
-python3 -c 'import DNAgen; DNAgen.DNAgen(1000, "DNAseq'$count'")'
+python3 -c 'import DNAgen; DNAgen.DNAgen(1000, "DNAseq'$count'")' # here we run DNAgen.py to create 100 .seq files
 
-Rscript BASE.r "DNAseq"$count 
+Rscript BASE.r "DNAseq"$count # here we run BASE.r to create 100 .count files
 
-sort -u DNAseq$count.count >> DNAseq.csv
+sort -u DNAseq$count.count >> DNAseq.csv # here we sort the outputs from the previous line into the file made on line 3
 
 done
  
